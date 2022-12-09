059bbf5626667a38850759bf79bfd445f6b15529
Avicoding <114409863+Avicoding@users.noreply.github.com>
2022-12-09 12:05:30 +0530

    Add files via upload

---
+++ char.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as mpl
+import numpy as np
+x=np.array([60,15,20,25,30,35,40,45,50,60,70,80,90,100,110])
+chart=["Avanish","vikas","Om","ahshan","roshan","Akash","Akash1","Sachin","ajay","ajay1","avanish1","vikash1","vikash2","vikash3","vikash4"]
+mpl.pie(x,labels=chart)
+mpl.show()
+print("Avanish mishra")
+a=int(input("inter a value"))
+print(a)
